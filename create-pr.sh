4e2b7d6beb313cf1e4fbd906c7c51119b6265136
Ivan Shcherbak <alotofall@gmail.com>
2019-08-19 08:01:04 +0300

    Exit successfully on negative condition

--- create-pr.sh
+++ create-pr.sh
@@ -22,7 +22,7 @@ if [[ "$1" != "" && "$2" != "" ]];  then
     condition=$(echo $data | grep $regex | wc -l )
     if [[ "$condition" == "0" ]]; then
       echo "✖ Negative condition. Stopping program"
-      exit 78
+      exit 0
     else
       echo "✓ Positive condition result. Continue"
     fi
